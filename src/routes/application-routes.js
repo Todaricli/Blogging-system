7d184cdc3d9186097fcc704a2e5042a0536d8c63
CarinaQi <qiyue1116@gmail.com>
2023-10-10 19:03:22 +1300

    route to sub page

--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -27,4 +27,10 @@ router.get('/login', function (req,res) {
     res.render('login');
 })
 
+router.get('/sub', function (req,res) {
+    // res.locals.subscriptionList = ["1","2","3","4"];
+    // res.locals.subscriberList = ["1","2","3","4","5"];
+    res.render('subscription&subscriber');
+})
+
 module.exports = router;
