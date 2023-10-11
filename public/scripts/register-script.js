c3411e01ea4a8c7fa0a8717935d0bf04a2a2becb
tonexie <tone.xie@gmail.com>
2023-10-12 12:07:27 +1300

    fix bugs

--- public/scripts/register-script.js
+++ public/scripts/register-script.js
@@ -2,26 +2,52 @@ window.addEventListener('load', function () {
     const usernameError = document.querySelector('#username-error');
     const usernameInput = document.querySelector('#username');
     usernameInput.addEventListener('input', async () => {
+        checkUsernameInDb();
+    });
+
+    const passwordFormatError = document.querySelector(
+        '#password-format-error'
+    );
+    const passwordMatchError = document.querySelector('#password-match-error');
+    const passwordInput = document.querySelector('#password');
+    const confirmPasswordInput = document.querySelector('#confirm-password');
+    passwordInput.addEventListener('input', async () => {
+        checkValidPasswordFormat();
+        checkPasswordsMatch();
+    });
+    confirmPasswordInput.addEventListener('input', async () => {
+        checkPasswordsMatch();
+    });
+
+    async function checkUsernameInDb() {
         const username = usernameInput.value;
-        const data = await checkUsernameInDb(username);
+        const response = await fetch(`/api/check-username?username=${username}`);
+        let data = await response.text();
         if (data === 'username exists') {
             usernameError.style.display = '';
             usernameError.innerHTML = 'Username exists, please choose another';
         } else usernameError.style.display = 'none';
-    });
+    }
 
-    const passwordError = document.querySelector('#password-error');
-    const passwordInput = document.querySelector('#password');
-    const confirmPasswordInput = document.querySelector('#confirm-password');
-    passwordInput.addEventListener('input', async () => checkPasswordsMatch());
-    confirmPasswordInput.addEventListener('input', async () =>
-        checkPasswordsMatch()
-    );
-
-    async function checkUsernameInDb(username) {
-        let response = await fetch(`/api/check-username?username=${username}`);
+    async function checkValidPasswordFormat() {
+        const password = passwordInput.value;
+        const response = await fetch(`/api/validate-password-format`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password }),
+        });
         let data = await response.text();
-        return data;
+        if (data === 'valid') {
+            passwordFormatError.style.display = 'none';
+            return true;
+        } else {
+            passwordFormatError.style.display = '';
+            passwordFormatError.innerHTML =
+                'Password must be at least 5 characters long and include at least 1 special character.';
+            return false;
+        }
     }
 
     async function checkPasswordsMatch() {
@@ -36,10 +62,10 @@ window.addEventListener('load', function () {
         });
         let data = await response.text();
         if (data === 'passwords match') {
-            passwordError.style.display = 'none';
+            passwordMatchError.style.display = 'none';
         } else {
-            passwordError.style.display = '';
-            passwordError.innerHTML = 'Please ensure passwords match';
+            passwordMatchError.style.display = '';
+            passwordMatchError.innerHTML = 'Please ensure passwords match';
         }
     }
 });
--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -14,9 +14,17 @@ router.get('/api/check-username', async function (req, res) {
 router.post('/api/check-passwords-match', async function (req, res) {
   const password = req.body.password
   const confirmPassword = req.body.confirmPassword
-  if (password === confirmPassword) {
+  if (password === confirmPassword || confirmPassword === '') {
     res.status(200).send('passwords match');
   } else res.status(400).send('passwords different');
+});
+
+router.post('/api/validate-password-format',  async function (req, res) {
+  const password = req.body.password;
+  const passwordRegex = /^(?=.*[\W_]).{5,}$/;
+  if (passwordRegex.test(password)) {
+    res.status(200).send('valid');
+  } else res.status(400).send('invalid');
 });
 
 module.exports = router;
