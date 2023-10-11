2c1db011765b89f278db1f69e23ad477386ecfc9
misssn1998 <siennanguyen0503@gmail.com>
2023-10-12 11:32:10 +1300

    Add a comma

--- src/models/generic-dao.js
+++ src/models/generic-dao.js
@@ -67,7 +67,7 @@ module.exports = {
     getUserDataByUsername,
     getAllUserData,
     getUserArticles,
-    getAllCommentsByArticles
+    getAllCommentsByArticles,
     getUserIdByUsername
 };
 
