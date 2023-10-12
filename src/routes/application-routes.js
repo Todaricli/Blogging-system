6b4793b6704b8d248ce177e50ac1d8f51a728354
Todaricli <leetony347@yahoo.com>
2023-10-12 22:25:59 +1300

    refactor write new article to new route file

--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -5,8 +5,6 @@ const testDao = require('../models/test-dao.js');
 const articleDao = require('../models/articles-dao.js');
 const genericDao = require('../models/generic-dao.js');
 
-const writeArticleDao = require('../models/writeArticle-dao.js');
-
 // router.get('/', async function (req, res) {
 //     res.locals.title = 'My route title!';
 //     res.locals.allTestData = await testDao.retrieveAllTestData();
@@ -82,32 +80,6 @@ router.post('/update_info', function(req,res) {
     res.locals.address = address;
 
     res.render('myProfile', {this: res.locals, information: updateInfo});
-})
-
-router.get("/writeArticle", function(req, res) {
-    res.render("writeArticle");
-})
-
-router.post("/postNewArticle", async function(req, res) {
-    const newArticle = req.body;
-
-    const user_id = res.locals.user.id;
-    const title = newArticle.titleKey;
-    const genre = newArticle.genreKey;
-    const content = newArticle.contentKey;
-    
-    console.log(content);
-    
-    let done = undefined;
-    done = await writeArticleDao.insertNewArticleToArticleTable(user_id, title, genre, content);
-
-    if(done) {
-        res.setToastMessage("New Article created!");
-    } else {
-        res.setToastMessage("Submitting error, try again!");
-    }
-    
-    res.redirect('/writeArticle');
 })
 
 module.exports = router;
---
+++ src/routes/new-article-routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const writeArticleDao = require('../models/writeArticle-dao.js');
+
+router.get("/writeArticle", function(req, res) {
+    res.render("writeArticle");
+})
+
+router.post("/postNewArticle", async function(req, res) {
+    const newArticle = req.body;
+
+    const user_id = res.locals.user.id;
+    const title = newArticle.titleKey;
+    const genre = newArticle.genreKey;
+    const content = newArticle.contentKey;
+    
+    console.log(content);
+    
+    let done = undefined;
+    done = await writeArticleDao.insertNewArticleToArticleTable(user_id, title, genre, content);
+
+    if(done) {
+        res.setToastMessage("New Article created!");
+    } else {
+        res.setToastMessage("Submitting error, try again!");
+    }
+    
+    res.redirect('/writeArticle');
+})
+
+module.exports = router;
