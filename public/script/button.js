2305a7bf17ea929d4a1271aafd953e0fd80c8d00
misssn1998 <siennanguyen0503@gmail.com>
2023-10-13 00:10:29 +1300

    like icon changes if user clicks

--- public/script/button.js
+++ public/script/button.js
@@ -21,14 +21,18 @@ closeComment.addEventListener("click", closeModal)
 //Create effect for like button
 
 const likeBtn = document.querySelector(".like-btn");
+const likeIcon = document.getElementById("like-icon");
+
 let isLiked = false;
 
 likeBtn.addEventListener("click", () => {
   isLiked = !isLiked;
 
-  if (isLiked) {
-    likeBtn.classList.add('liked');
-  } else {
-    likeBtn.classList.remove('liked');
-  }
+ if (isLiked) {
+        likeIcon.classList.replace("fa-regular", "fa-solid");
+        likeIcon.classList.add("liked");
+    } else {
+        likeIcon.classList.replace("fa-solid", "fa-regular");
+        likeIcon.classList.remove("liked");
+    }
 })
