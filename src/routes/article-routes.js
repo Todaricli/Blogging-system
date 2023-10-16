d519f3b42954f1f6df79c44372c7233022974c6e
misssn1998 <siennanguyen0503@gmail.com>
2023-10-17 02:25:58 +1300

    create calendar and filter articles based on calendar dates

--- src/routes/article-routes.js
+++ src/routes/article-routes.js
@@ -167,44 +167,22 @@ function convertDeltaToHtml(content) {
   return contentArray;
 }
 
-// router.get('/article/:id', comment.generateComments,async function (req, res) {
-//     const article_id = req.params.id;
+router.get("/calendar", async function(req,res) {
 
-//     const article = await articleDao.getArticlesByID(article_id);
-//     //console.log(article)
-//     res.locals.article = article;
-//     const content = article[0].content;
+  res.render("test");
+})
 
-//     try {
-//         const content_obj = JSON.parse(content);
+router.get('/filtered-articles', async function(req, res) {
+  const {startDate, endDate}  = req.query;
+  console.log(startDate);
+  console.log(endDate);
 
-//         const delta_obj = content_obj.ops;
+  const articles = await articleDao.filterArticlesBySelectedDates(startDate, endDate);
+  console.log(articles);
 
-//         const cfg = {
-//             inlineStyles: true,
-//             multiLineBlockquote: true,
-//             multiLineHeader: true,
-//         };
+  res.locals.articles = articles;
+  res.render("filteredArticle")
 
-//         //const converter = new QuillDeltaToHtmlConverter(delta_obj, cfg);
-
-//         const html = converter.convert();
-//         res.locals.article_content = html;
-//     } catch (error) {
-//         const html = '<p>Article loading error! refresh the page.<p>';
-//         res.locals.article_content = html;
-//     }
-
-//     const authorName = await articleDao.getAuthorByArticle(article_id);
-//     //console.log(authorName);
-//     res.locals.authorName = authorName;
-    
-//     const likeCounts = await articleDao.getNumberOfLikesFromArticle(article_id);
-//     //console.log(likeCounts);
-//     res.locals.like_count = likeCounts;
-
-//     res.render('articleDemo');
-// });
-
+})
 
 module.exports = router;
