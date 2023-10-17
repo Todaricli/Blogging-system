19afa76579718e85c56c75ccd46f044c0eb22fa2
Todaricli <leetony347@yahoo.com>
2023-10-17 14:03:05 +1300

    can comment feature front-end

--- public/script/comment.js
+++ public/script/comment.js
@@ -1,4 +1,12 @@
 window.addEventListener("load", function () {
 });
 
-async function 
+async function verifyLogInStatus() {
+  const user_id = document.getElementById("user_id_temp_storage").value;
+
+  if(!user_id) {
+    return;
+  }
+
+  
+}
