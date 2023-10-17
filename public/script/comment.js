7ba7aeefcc0640efc454f89a08c504ec1dde61b3
Todaricli <leetony347@yahoo.com>
2023-10-17 23:11:24 +1300

    Add front-end for comment feature

--- public/script/comment.js
+++ public/script/comment.js
@@ -1,12 +1,38 @@
 window.addEventListener("load", function () {
+  verifyLogInStatus();
+  commentsOnArticle();
+  commentOnComment();
 });
 
-async function verifyLogInStatus() {
+function verifyLogInStatus() {
   const user_id = document.getElementById("user_id_temp_storage").value;
 
   if(!user_id) {
-    return;
+    console.log(user_id);
+    document.getElementById("comments-div").hidden = true;
   }
+}
 
-  
+function commentsOnArticle() {
+  const form = document.getElementById("comment_on_article_form");
+
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    let textarea = document.querySelector('textarea').value;
+    console.log(textarea);
+  })
+}
+
+function commentOnComment() {
+  const forms = document.querySelectorAll(".comment_on_comment_form");
+
+  forms.forEach(form => {
+    form.addEventListener('submit', async function(e) {
+      e.preventDefault();
+      let textarea = form.querySelector('textarea').value
+      let comment_id = form.querySelector('input[name="comment_id_storage"]').value
+      console.log(textarea);
+      console.log(comment_id);
+    })
+  })
 }
