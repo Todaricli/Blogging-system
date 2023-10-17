019f2ffd220c5a90949c5a807c47d53391dd9812
CarinaQi <qiyue1116@gmail.com>
2023-10-17 15:19:33 +1300

    remove sub routes to sub-api

--- src/routes/api/sub-api.js
+++ src/routes/api/sub-api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const subDao = require('../../models/sub-dao');
+const { verifyAuthenticated } = require('../../middleware/auth-middleware/login-auth.js');
 
 router.post('/api/checkIfSub', async function (req, res) {
     const user_id = req.body.user_id;
@@ -9,5 +10,51 @@ router.post('/api/checkIfSub', async function (req, res) {
     const result = await subDao.checkIfSubscribe(user_id, check_id);
     res.status(200).send(result);
 });
+
+
+router.get("/removeSubscription", verifyAuthenticated, async function (req, res) {
+    const subscription_id = req.query.id;
+    const user_id = res.locals.user.id;
+    if (user_id) {
+        try {
+          await subDao.removeSpecificSubscriptionByID(user_id, subscription_id);
+          res.status(200).json({ message: 'Subscription removed successfully' });
+        } catch (error) {
+          res.status(500).json({ message: 'Error removing subscription' });
+        }
+      } else {
+        res.status(403).json({ message: 'Unauthorized' });
+      }
+})
+
+router.get("/addSubscription", verifyAuthenticated, async function (req, res) {
+    const subscription_id = req.query.id;
+    const user_id = res.locals.user.id;
+    if (user_id) {
+        try {
+          await subDao.addSpecificSubscriptionByID(user_id, subscription_id);
+          res.status(200).json({ message: 'Subscription removed successfully' });
+        } catch (error) {
+          res.status(500).json({ message: 'Error removing subscription' });
+        }
+      } else {
+        res.status(403).json({ message: 'Unauthorized' });
+      }
+})
+
+router.get("/removeSubscriber", verifyAuthenticated, async function (req, res) {
+    const subscriber_id = req.query.id;
+    const user_id = res.locals.user.id;
+    if (user_id) {
+        try {
+          await subDao.removeSpecificSubscriberByID(user_id, subscriber_id);
+          res.status(200).json({ message: 'Subscription removed successfully' });
+        } catch (error) {
+          res.status(500).json({ message: 'Error removing subscription' });
+        }
+      } else {
+        res.status(403).json({ message: 'Unauthorized' });
+      }
+})
 
 module.exports = router;
