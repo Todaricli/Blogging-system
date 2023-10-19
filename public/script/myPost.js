20115cdf5db363d539924584352190ebec564efd
Alden <amai111@aucklanduni.ac.nz>
2023-10-19 11:17:37 +0800

    added delete article with front end rendering

--- public/script/myPost.js
+++ public/script/myPost.js
@@ -1,19 +1,29 @@
 window.addEventListener(`load`, async function(){
 
-    const deleteButton = document.querySelector(".delete-button")
+    const deleteButton = document.querySelectorAll(".delete-button")
+    const articleCount = document.querySelector("#article-count-header")
+    const dropDownDiv = document.querySelector(".each-posts-links")
 
-    deleteButton.addEventListener(`click`, async(e)=>{
-        let articleId = e.target.getAttribute("article-id")
-        console.log(articleId)
-        if(confirm("are you sure you want to delete this article?")){
-            await deleteArticle(articleId)
-            txt = "article deleted "
-        }else{
-            txt = "cancelled"
-        }
+    let articleCounter = deleteButton.length
+
+    deleteButton.forEach((e,i)=>{
+        deleteButton[i].addEventListener(`click`, async(e)=>{
+            let articleId = e.target.getAttribute("article-id")
+            if(confirm("are you sure you want to delete this article?")){
+                await deleteArticle(articleId)
+                const elementToRemove =document.querySelector(`#article${articleId}`)
+                articleCounter--
+                elementToRemove.innerHTML = "<h2>article has been deleted</h2>"
+                articleCount.innerHTML = `your stories(${articleCounter})`
+            }else{
+                txt = "cancelled"
+            }
+        })
     })
+    
 
     async function deleteArticle(id){
         await fetch(`/api/delete-article?articleId=${id}`)
+        alert("Article deleted")
     }
 })
